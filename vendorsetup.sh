ca52432fc25778ff887f209fbee7916c1d959604
drpto <dr.juniornoob@gmail.com>
2024-08-16 09:02:09 +0530

    fogos: Track clang-17 from https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r487747c.git
    Signed-off-by: drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,7 @@ git clone https://github.com/Fogos-Trees/kernel_motorola_sm6375.git kernel/motor
 
 rm -rf hardware/motorola
 git clone https://github.com/LineageOS/android_hardware_motorola.git -b lineage-21 hardware/motorola
+
+rm -rf prebuilts/clang/host/linux-x86/clang-r487747c
+git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r487747c.git -b 14.0  prebuilts/clang/host/linux-x86/clang-r487747c
+
