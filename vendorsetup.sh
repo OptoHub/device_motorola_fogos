886c1cf439c55167861c9a7e0edbdbe51661e609
Drpto <dr.juniornoob@gmail.com>
2024-11-02 06:07:15 +0000

    fogos: Track right repo branches for A15
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-git clone https://github.com/Fogos-Trees/device_xiaomi_sm6375-common.git device/motorola/sm6375-common
-git clone https://github.com/Fogos-Trees/vendor_motorola_fogos.git vendor/motorola/fogos
-git clone https://github.com/Fogos-Trees/vendor_motorola_sm6375-common.git vendor/motorola/sm6375-common
-git clone https://github.com/Fogos-Trees/kernel_motorola_sm6375.git kernel/motorola/sm6375
+git clone https://github.com/OptoHub/device_motorola_sm6375-common.git -b 15 device/motorola/sm6375-common
+git clone https://github.com/OptoHub/vendor_motorola_fogos.git -b 15 vendor/motorola/fogos
+git clone https://github.com/OptoHub/vendor_motorola_sm6375-common.git -b 15 vendor/motorola/sm6375-common
+git clone https://github.com/OptoHub/kernel_motorola_sm6375.git -b 15 kernel/motorola/sm6375
 
 rm -rf hardware/motorola
-git clone https://github.com/LineageOS/android_hardware_motorola.git -b lineage-21 hardware/motorola
+git clone https://github.com/OptoHub/hardware_motorola.git -b 15 hardware/motorola
 
 rm -rf prebuilts/clang/host/linux-x86/clang-r487747c
 git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r487747c.git -b 14.0  prebuilts/clang/host/linux-x86/clang-r487747c
