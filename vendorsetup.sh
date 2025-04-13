9e7299f31a8646dacb4ba49fa2e7a016932838ea
Drpto <dr.juniornoob@gmail.com>
2025-04-13 20:29:16 +0000

    fogos: Teack clamg 19.x.x from https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,8 +6,8 @@ git clone https://github.com/OptoHub/kernel_motorola_sm6375.git -b 15 kernel/mot
 rm -rf hardware/motorola
 git clone https://github.com/OptoHub/hardware_motorola.git -b 15 hardware/motorola
 
-rm -rf prebuilts/clang/host/linux-x86/clang-r487747c
-git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r487747c.git -b 14.0  prebuilts/clang/host/linux-x86/clang-r487747c
+rm -rf prebuilts/clang/host/linux-x86
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android15-qpr2-release prebuilts/clang/host/linux-x86
 
 rm -rf vendor/motorola/fogos/proprietary/product/priv-app/MotCamera4
 mkdir vendor/motorola/fogos/proprietary/product/priv-app/MotCamera4 && cd vendor/motorola/fogos/proprietary/product/priv-app/MotCamera4
