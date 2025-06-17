471a47786a8d357efd495d33e0ca138b6006f631
Drpto <dr.juniornoob@gmail.com>
2025-06-17 13:20:43 +0000

    fogos: Track right branch for A16
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,13 @@
-git clone https://github.com/OptoHub/device_motorola_sm6375-common.git -b 15 device/motorola/sm6375-common
-git clone https://github.com/OptoHub/vendor_motorola_fogos.git -b 15 vendor/motorola/fogos
-git clone https://github.com/OptoHub/vendor_motorola_sm6375-common.git -b 15 vendor/motorola/sm6375-common
-git clone https://github.com/OptoHub/kernel_motorola_sm6375.git -b 15 kernel/motorola/sm6375
+git clone https://github.com/OptoHub/device_motorola_sm6375-common.git -b 16 device/motorola/sm6375-common
+git clone https://github.com/OptoHub/vendor_motorola_fogos.git -b 16 vendor/motorola/fogos
+git clone https://github.com/OptoHub/vendor_motorola_sm6375-common.git -b 16 vendor/motorola/sm6375-common
+git clone https://github.com/OptoHub/kernel_motorola_sm6375.git -b 16 kernel/motorola/sm6375
 
 rm -rf hardware/motorola
 git clone https://github.com/OptoHub/hardware_motorola.git -b 15 hardware/motorola
 
 rm -rf prebuilts/clang/host/linux-x86
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android15-qpr2-release prebuilts/clang/host/linux-x86 --depth=1
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android16-release prebuilts/clang/host/linux-x86 --depth=1
 
 rm -rf vendor/motorola/fogos/proprietary/product/priv-app/MotCamera4
 mkdir vendor/motorola/fogos/proprietary/product/priv-app/MotCamera4 && cd vendor/motorola/fogos/proprietary/product/priv-app/MotCamera4
