074e2f656e139e22631afb9e1b52c73cf13a4363
Drpto <dr.juniornoob@gmail.com>
2025-07-21 05:19:55 +0000

    fogos: Update from V1UG35H.75-14-9
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- infinity_fogos.mk
+++ infinity_fogos.mk
@@ -43,6 +43,6 @@ PRODUCT_MODEL := moto g34 5G
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="fogos_g-user 14 U1UGS34M.23-82-2-6 0b9749 release-keys" \
-    BuildFingerprint=motorola/fogos_g/fogos:11/U1UGS34.23-82-2-6/e21904:user/release-keys \
+    BuildDesc="fogos_g-user 15 V1UG35H.75-14-9 abc528-11988f release-keys MV-186" \
+    BuildFingerprint=motorola/fogos_g/fogos:15/V1UG35H.75-14-9/abc528-11988f:user/release-keys \
     DeviceProduct=fogos_g
